8aa19324c03d72853d0f26e5f45f881c29c78ccb
Zai <fotima.yakubova2005@gmail.com>
2025-01-07 10:42:24 +0500

    Add Products app in admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Products
+
+#admin.site.register(Products)
+
+@admin.register(Products)
+class ProductsAdmin (admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    name= models.CharField(max_length=150, unique=True, verbose_name='Название')
+    slug=models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description =models.TextField(blank=True,null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='index_images', blank=True, null=True, verbose_name='Изображение')
+    price=models.DecimalField(default =0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
+    discount=models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка в %')
+    quantity=models.PositiveIntegerField(default=0, verbose_name='Количество')
+
+
+    class Meta:
+        db_table: str = 'product'
+        verbose_name: str='Продукт'
+        verbose_name_plural: str ='Продукты'
+
+    def __str__(self) -> str:
+        return f'{self.name} Количество -{self.quantity}'
